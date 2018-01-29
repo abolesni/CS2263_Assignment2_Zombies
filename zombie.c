8abf1188e4012ad6d914e38d5b0f34760bedaec3
Adrian Bolesnikov <Adrian.Bolesnikov@unb.ca>
2018-01-29 16:57:54 -0400

    Initial commit

---
+++ zombie.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void set_zombies(int n, char fred[n][n]){
+}
+void print_zombies(int n, char fred[n][n]){
+}
+void block_zombies(int n, char fred[n][n]){
+}
+
+int main(){
+}
