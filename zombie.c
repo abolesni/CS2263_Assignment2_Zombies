d2dea7eb66bdabd1edfa5e50ff312ba0021ac7fc
Adrian Bolesnikov <Adrian.Bolesnikov@unb.ca>
2018-01-30 19:28:58 -0400

    Added set_zombies functionality

--- zombie.c
+++ zombie.c
@@ -1,7 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void set_zombies(int n, char fred[n][n]){
+void set_zombies(int n, char fred[n+2][n+2]){
+	int row;
+	int col;
+	while(scanf("%d %d", &row, &col) == 2){
+		if (row < 0 || col < 0 || row >= n || col >= n){
+			printf("Invalid input. Exiting");
+			break;
+		}
+		row++;
+		col++;
+		fred[row][col] = 'Z';
+	}
 }
 void print_zombies(int n, char fred[n][n]){
 }
@@ -9,4 +20,29 @@ void block_zombies(int n, char fred[n][n]){
 }
 
 int main(){
+	//Dimension n being inputted
+	int n;
+	scanf("%d", &n);
+	char fred[n+2][n+2];
+	
+	//Pad array
+	for (int i = 0; i < n+2; i++){
+		for (int j = 0; j < n+2; j++){
+			if (i == 0 || i == 11 || j == 0 || j == 11){
+				fred[i][j] = 'X';
+			}
+			else{
+				fred[i][j] = '.';
+			}
+		}
+	}
+	
+	set_zombies(n, fred);
+	for (int i = 0; i < n+2; i++){
+		for (int j = 0; j < n+2; j++){
+			printf("%c", fred[i][j]);
+		}
+		printf("\n");
+	}
+
 }
